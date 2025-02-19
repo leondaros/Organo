d005de753fef0f71f2dc45989c57cb3fb6701ccc
Leon Daros <leondaros@gmail.com>
2025-02-19 23:06:28 +0000

    Create Team

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,10 +6,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação','Front-End','Data Science','Devops','UX e Design','Mobile','Inovação e Gestão'
-    ]
-
     const [nome,setNome] = useState('')
     const [cargo,setCargo] = useState('')
     const [imagem,setImagem] = useState('')
@@ -52,7 +48,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     required={true} 
                     label="Time" 
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
