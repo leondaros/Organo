52a269fdeb3e510f5d991421a425e450eadcc8ae
Leon Daros <leondaros@gmail.com>
2025-02-22 11:18:00 +0000

    Create team member

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/leondaros.png" alt="Leon Daros"/>
+            </div>
+            <div className='rodape'>
+                <h4>Leon Daros</h4>
+                <h5>Product Manager</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
